b4996f42c2492c587c61c095b80cf75d377b9764
Blemtoze <savin.lev.and@gmail.com>
2024-06-24 00:51:13 +0000

    Script update

--- script.sh
+++ script.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-python -m venv env
+python -m venv env #Создаём виртуальное окружение
 
-source env/bin/activate
+source env/bin/activate #Запускаем его
 
-pip install -r requirements.txt
+pip install -r requirements.txt #Устанавливаем необходимые зависимости
 
-rm -r accounts/migrations/00*
+rm -r accounts/migrations/00* #Удаляем ненужные миграции
 
-rm db.sqlite3
+rm db.sqlite3 #Удаляем БД
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
+python manage.py makemigrations #Выполняем первичный список миграций
 
-python manage.py makemigrations
+python manage.py migrate #Выполняем эти миграции
 
-python manage.py migrate
+username="admin"
+email="admin@example.com"
+password="admin"
 
-python manage.py runserver 0.0.0.0:8000
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$username', '$email', '$password')" | python manage.py shell #Создаём суперпользователя с данными, указанными тут ранее
+
+python PREDICT.py pred.xlsx #Выполняем предсказание на основе файла pred.xlsx
+
+python LOAD_EMP.py doc.xlsx login.txt #Создаём аккаунты для всех работников из doc.xlsx и временно записываем их данные в login.txt
+
+python create_schedule.py #Создаём расписание для врачей
+
+python manage.py runserver 0.0.0.0:8000 #Запускаем сервер по адресу 0.0.0.0:8000
