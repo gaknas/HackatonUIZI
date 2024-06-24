9bf0d6fd804d45d308a4425fd32ae147bb26b743
Blemtoze <91263805+Blemtoze@users.noreply.github.com>
2024-06-24 11:30:18 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-python -m venv env #Создаём виртуальное окружение
+python3 -m venv env #Создаём виртуальное окружение
 
 source env/bin/activate #Запускаем его
 
-pip install -r requirements.txt #Устанавливаем необходимые зависимости
+pip3 install -r requirements.txt #Устанавливаем необходимые зависимости
 
 rm -r accounts/migrations/00* #Удаляем ненужные миграции
 
 rm db.sqlite3 #Удаляем БД
 
-python manage.py makemigrations #Выполняем первичный список миграций
+python3 manage.py makemigrations #Выполняем первичный список миграций
 
-python manage.py migrate #Выполняем эти миграции
+python3 manage.py migrate #Выполняем эти миграции
 
 username="admin"
 email="admin@example.com"
 password="admin"
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$username', '$email', '$password')" | python manage.py shell #Создаём суперпользователя с данными, указанными тут ранее
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$username', '$email', '$password')" | python3 manage.py shell #Создаём суперпользователя с данными, указанными тут ранее
 
-python PREDICT.py pred.xlsx #Выполняем предсказание на основе файла pred.xlsx
+python3 PREDICT.py pred.xlsx #Выполняем предсказание на основе файла pred.xlsx
 
-python LOAD_EMP.py doc.xlsx login.txt #Создаём аккаунты для всех работников из doc.xlsx и временно записываем их данные в login.txt
+python3 LOAD_EMP.py doc.xlsx login.txt #Создаём аккаунты для всех работников из doc.xlsx и временно записываем их данные в login.txt
 
-python create_schedule.py #Создаём расписание для врачей
+python3 create_schedule.py #Создаём расписание для врачей
